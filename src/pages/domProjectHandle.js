affe28a9246f462c0c0cde2df6a77b6d17201420
tajwararik <tajwar.arik.10@gmail.com>
2024-08-24 19:50:49 +0600

    Created project cards and added to the display

--- src/pages/domProjectHandle.js
+++ src/pages/domProjectHandle.js
@@ -2,8 +2,15 @@ import { projectArray } from "./createProject";
 
 export const loadDomProjectHandle = function () {
   const display = document.querySelector(".container > div:last-of-type");
+  display.innerHTML = "";
 
   for (let project of projectArray) {
     const projectCard = document.createElement("div");
+    projectCard.classList.add("project-card");
+
+    projectCard.innerHTML = `<h2>${project.name}</h2>
+    <p>Created: ${project.date}</p>`;
+
+    display.append(projectCard);
   }
 };
