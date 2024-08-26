648324df5633e3eecc21a281c1cea0989f619a22
tajwararik <tajwar.arik.10@gmail.com>
2024-08-27 02:45:42 +0600

    Make cards from taskArray elements

--- src/pages/domTaskHandle.js
+++ src/pages/domTaskHandle.js
@@ -1,1 +1,21 @@
+import { taskArray } from "./createTask";
+import "./taskCards.css";
 
+export const loadDomTaskHandle = function () {
+  const display = document.querySelector(
+    ".display-container > div:last-of-type"
+  );
+  display.innerHTML = "";
+
+  for (let task of taskArray) {
+    const taskCard = document.createElement("div");
+    taskCard.classList.add("task-card");
+
+    taskCard.innerHTML = `<h3>${task.title}</h3>
+    <p>Description: ${task.description}</p>
+    <p>Due: ${task.dueDate}</p>
+    <p>Priority: ${task.priority}`;
+
+    display.append(taskCard);
+  }
+};
