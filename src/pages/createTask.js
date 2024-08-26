25bde802d18988350e1831c632092dc98915f58f
tajwararik <tajwar.arik.10@gmail.com>
2024-08-26 19:20:23 +0600

    Create object for tasks

---
+++ src/pages/createTask.js
@@ -0,0 +1,40 @@
+const taskContainer = document.querySelector(".task-container");
+const taskDialog = taskContainer.querySelector("dialog");
+const form = taskDialog.querySelector("form");
+
+class Task {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+export function createTask() {
+  const taskTitle = form.querySelector("#title").value;
+  const taskDescription = form.querySelector("#description").value;
+  const taskDueDate = form.querySelector("#due-date").value;
+  let taskPriority = "";
+
+  form.querySelectorAll("div input[type='radio']").forEach((radio) => {
+    if (radio.checked) taskPriority = radio.value;
+  });
+
+  const newTask = new Task(
+    taskTitle,
+    taskDescription,
+    taskDueDate,
+    taskPriority
+  );
+
+  if (
+    taskTitle !== "" &&
+    taskDescription !== "" &&
+    taskDueDate !== "" &&
+    taskPriority !== ""
+  )
+    taskDialog.close();
+
+  form.reset();
+}
