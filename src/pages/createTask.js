7658f554a5b5f3110d70f70dfd782efa324a907b
tajwararik <tajwar.arik.10@gmail.com>
2024-09-01 20:42:22 +0600

    Added default tasks to task array

--- src/pages/createTask.js
+++ src/pages/createTask.js
@@ -5,7 +5,48 @@ const taskContainer = document.querySelector(".task-container");
 const taskDialog = taskContainer.querySelector("dialog");
 const form = taskDialog.querySelector("form");
 
-export const taskArray = [];
+export const taskArray = [
+  {
+    title: "Grocery Shopping",
+    description: "Buy vegetables, fruits, and dairy products for the week.",
+    dueDate: format(new Date(), "dd MMM yyyy"),
+    project: "General",
+    priority: "Medium",
+  },
+
+  {
+    title: "Schedule Doctor's Appointment",
+    description: "Book an appointment with the dentist for a routine check-up.",
+    dueDate: format(new Date(), "dd MMM yyyy"),
+    project: "General",
+    priority: "High",
+  },
+
+  {
+    title: "Clean Living Room",
+    description:
+      "Vacuum the carpet, dust the furniture, and tidy up the living room.",
+    dueDate: format(new Date(), "dd MMM yyyy"),
+    project: "General",
+    priority: "Low",
+  },
+
+  {
+    title: "Pay Bills",
+    description: "Pay the electricity and internet bills before the due date.",
+    dueDate: format(new Date(), "dd MMM yyyy"),
+    project: "General",
+    priority: "High",
+  },
+
+  {
+    title: "Call Mom",
+    description: "Catch up with Mom and check in on how she’s doing.",
+    dueDate: format(new Date(), "dd MMM yyyy"),
+    project: "General",
+    priority: "Medium",
+  },
+];
 
 class Task {
   constructor(title, description, dueDate, project, priority) {
