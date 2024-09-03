9ff56e1811f87d4524515d4e9b264595d1e7a4a3
tajwararik <tajwar.arik.10@gmail.com>
2024-09-04 01:21:42 +0600

    Created the functions

--- src/pages/tasksInProjects.js
+++ src/pages/tasksInProjects.js
@@ -1,4 +1,12 @@
 import { taskArray } from "./createTask";
 import { loadTasksInProject } from "./domTasksInProjectHandle";
 
+export function handleTasksInProjects(projectName) {
+  searchProject(projectName);
+}
 
+function searchProject(projectName) {
+  loadTasksInProject(
+    taskArray.filter((projects) => projects.project.includes(projectName))
+  );
+}
